1f572f4529c5a4f28d83c0c10f15097e7f47a3f7
sinanazem <sinanaazem@gmail.com>
2024-01-19 00:17:29 +0330

    update

--- projects/MetalDashboard/src/pipe/extract.py
+++ projects/MetalDashboard/src/pipe/extract.py
@@ -1,9 +1,34 @@
 import requests
 import json
 
-def get_metal_data():
-    url = f"{}"
-    response = requests.get(url)
-    if response.ok:
-        data = response.json()
-    return data
+def make_gapi_request():
+    api_key = "goldapi-4a1o6rlrk555jk-io"
+    symbol = "XAU"
+    curr = "USD"
+    date = ""
+
+    url = f"https://www.goldapi.io/api/{symbol}/{curr}{date}"
+    
+    headers = {
+        "x-access-token": api_key,
+        "Content-Type": "application/json"
+    }
+    
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+
+        result = response.text
+        print(result)
+    except requests.exceptions.RequestException as e:
+        print("Error:", str(e))
+
+make_gapi_request()
+
+
+# def get_metal_data():
+#     url = f"{}"
+#     response = requests.get(url)
+#     if response.ok:
+#         data = response.json()
+#     return data
