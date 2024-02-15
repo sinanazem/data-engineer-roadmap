faef25df7502b834d9259c4765664aa16b2806ec
sinanazem <sinanaazem@gmail.com>
2024-02-15 05:36:05 +0330

    update

---
+++ rabbitmq/receiver.py
@@ -0,0 +1,14 @@
+import pika 
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
+ch2 = connection.channel()
+
+ch2.queue_declare(queue="hello")
+
+def callback(ch, method, properties, body):
+    print(f" Received: {body}")
+
+ch2.basic_consume(queue="hello", on_message_callback=callback, auto_ack=True)
+
+print("waiting for message for wxit please ctrl+c: ")
+ch2.start_consuming()
---
+++ rabbitmq/sender.py
@@ -0,0 +1,11 @@
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
+
+ch1 = connection.channel()
+
+ch1.queue_declare("hello")
+ch1.basic_publish(exchange="", routing_key="hello", body="hello world")
+print("message sent!")
+
+connection.close()
